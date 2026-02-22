66bc50994116e1966516900932eb5d4face54115
NuuttiSir <nuutti.sirvio@gmail.com>
2026-02-22 19:11:34 +0200

    made stuff

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpHandler;
@@ -22,10 +23,16 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        UserAuthentication userAuth = new UserAuthentication();
 
         server.createContext("/", new handler1());
-        server.createContext("/msg", new handler2());
-        server.createContext("/registration", new handlerRegisteration());
+        HttpContext httpContextMsgFeed = server.createContext("/msgFeed", new handler2());
+
+        HttpContext httpContextRegistration = server.createContext("/registration",
+                new UserRegistrationHandler(userAuth));
+
+        httpContextMsgFeed.setAuthenticator(userAuth);
+
         server.setExecutor(Executors.newCachedThreadPool());
         server.start();
     }
@@ -124,61 +131,5 @@ public class Main {
         }
     }
 
-    public static class handlerRegisteration implements HttpHandler {
-
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-            System.out.println("IN REG");
-            Headers headers = exchange.getRequestHeaders();
-            String contentType = "";
-            String response = "";
-            int responseCode = 404;
-
-            if (exchange.getRequestMethod().equalsIgnoreCase("POST")) {
-                if (headers.containsKey("Content-Type")) {
-                    contentType = headers.get("Content-Type").get(0);
-                }
-
-                if (contentType.equalsIgnoreCase("text/plain")) {
-                    BufferedReader bfReader = new BufferedReader(
-                            new InputStreamReader(exchange.getRequestBody(), StandardCharsets.UTF_8));
-                    String[] usernamePassword = bfReader.lines().collect(Collectors.joining("\n")).split(":");
-
-                    String username = usernamePassword[0];
-                    String password = usernamePassword[1];
-
-                    response = "Username + " + username + " password " + password;
-                    byte[] bytes = response.getBytes("UTF-8");
-                    exchange.sendResponseHeaders(200, bytes.length);
-                    OutputStream outputStream = exchange.getResponseBody();
-                    outputStream.write(response.getBytes());
-
-                    outputStream.flush();
-                    outputStream.close();
-                }
-            } else if (exchange.getRequestMethod().equalsIgnoreCase("GET")) {
-                String regSiteHTML = """
-                        <html>
-
-                        <body>
-                        	<h1>REG</h1>
-                        	<pre>Cant GET in REG dummy</pre>
-                        </body>
-
-                        </html>
-                            """;
-                byte[] bytes = regSiteHTML.getBytes("UTF-8");
-
-                exchange.sendResponseHeaders(200, bytes.length);
-
-                OutputStream outputStream = exchange.getResponseBody();
-                outputStream.write(regSiteHTML.getBytes());
-
-                outputStream.flush();
-                outputStream.close();
-            } else {
-                return;
-            }
-        }
-    }
+    // TODO: Thinking should I do a kind of whoami that return username nad stuff
 }
